65698b229808831b3d23e9359c6fe02816b88230
Lucy Deacon <ogdenface@gmail.com>
2022-07-27 11:13:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# A Toki Pona system for Plover #
+
+This is a WIP 10+2 key system for typing Toki Pona.
+
+## Layout ##
+
+![Toki Pona JNL Layout](toki_pona_jnl_layout.png)
+
+In this system, each word in Toki Pona is typed as a unique 3-letter code, generally corresponding to the first three letters. For example: 'toki' is TOK, 'kepeken' is KEP, etc.
+
+Syllables for loanwords are typed similarly, using the extra functions in the right hand.
+
+For example:
+ilo Senokapi li pona tawa mi a
+
+
+Currently working on support for sitelen pona fonts such as linja pona.
+
---
+++ plover-toki-pona-jnl/system.py
@@ -0,0 +1,51 @@
+# STML#AO*LMNW
+
+KEYS = (
+	'S-', 'T-', 'M-', 'L-', '#',
+	'A-', '-O',
+	'*', '-L', '-M', '-N', '-W'
+)
+
+IMPLICIT_HYPHEN_KEYS = (
+	'A-', '-O'
+)
+
+NUMBER_KEY = None
+
+NUMBERS = {}
+
+UNDO_STROKE_STENO = None
+
+SUFFIX_KEYS = ()
+
+KEYMAPS = {
+	'Keyboard': {
+		'S-': 'q',
+		'T-': 'w',
+		'M-': 'e',
+		'L-': 'r',
+		'#': 't',
+		'A-': 'v',
+		'-O': 'n',
+		'*': 'y',
+		'-L': 'u',
+		'-M': 'i',
+		'-N': 'o',
+		'-W': 'p',
+		'arpeggiate': 'space',
+		'no-op': ('[','a','s','d','f','g','h','j','k','l',';','\'','z','x','c','b','m',',','.','/'),
+	}
+}
+
+ORTHOGRAPHY_RULES = []
+
+DICTIONARIES_ROOT = 'asset:plover_toki_pona_jnl:dictionaries'
+
+DEFAULT_DICTIONARIES = (
+	'kipisi_nimi.json', 'nimi_ku_suli.json', 'sitelen.json'
+)
+
+ORTHOGRAPHY_RULES_ALIASES = {}
+
+ORTHOGRAPHY_WORDLIST = None
+
